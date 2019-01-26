3c678def5be5bd93189916c3f8c866a367bb330b
blackwell <blackwellsmithiv@gmail.com>
2019-01-26 20:08:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,8 +56,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supply|
     puts "  #{holiday.split('_').capitalize.join(' ')}: #{supply.join(", ")}"
+    end
   end
-end
 end
 
 def all_holidays_with_bbq(holiday_hash)
